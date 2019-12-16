3478631d9304d443943fc5a5923176264ff6cacf
Ravindran Jayabalan <ravindran.jayabalan1@ibm.com>
2019-12-16 16:46:01 +0800

    change X
    Signed-off-by: Ravindran Jayabalan <ravindran.jayabalan1@ibm.com>

--- src/wasdev/sample/SimpleServlet.java
+++ src/wasdev/sample/SimpleServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SimpleServlet
+ * Servlet implementation class SimpleServletX
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
