75bd30ecc7e5d69d6ba35b32ed069a9164772b27
Ravindran Jayabalan <ravindran.jayabalan1@ibm.com>
2019-12-18 12:41:58 +0800

    added 123

--- src/wasdev/sample/SimpleServlet.java
+++ src/wasdev/sample/SimpleServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SimpleServletX
+ * Servlet implementation class SimpleServletX123
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
